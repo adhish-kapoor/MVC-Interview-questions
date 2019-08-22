b3a3190f34ea9bdadfacd00e13606f4e2b971be8
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2019-08-22 14:53:14 +0530

    Update C#_Concepts.cs

--- C#_Concepts.cs
+++ C#_Concepts.cs
@@ -66,3 +66,15 @@ Boxing a value type allocates an object instance on the heap and copies the valu
     
 UNBOXING
 Unboxing is used to get value from object type.
+    
+=======Singleton Design Pattern
+
+The Singleton design pattern is one of the simplest design patterns.
+This pattern ensures that the class has only one instance and provides a global point of access to it. 
+The pattern ensures that only one object of a specific class is ever created. 
+All further references to objects of the singleton class refer to the same underlying instance.
+
+There are situations in a project where we want only one instance of the object to be created and shared among the clients. 
+No client can create an instance from outside.
+It is more appropriate than creating a global variable since this may be copied and leads to multiple access points. 
+    
