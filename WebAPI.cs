eb950e1beea675dee44a595093e4042889d28640
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2019-09-24 15:21:13 +0530

    Create WebAPI.cs

---
+++ WebAPI.cs
@@ -0,0 +1,2 @@
+//Web API can be accessed in the server side code in .NET and also on client side using JavaScript frameworks such as jQuery, AnguarJS, KnockoutJS etc.
+========Consume Web API in ASP.NET MVC
