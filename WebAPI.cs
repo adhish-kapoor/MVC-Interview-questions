d46e36b56a216d16eecddbc0514523fad236cad8
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2019-09-24 15:25:21 +0530

    Update WebAPI.cs

--- WebAPI.cs
+++ WebAPI.cs
@@ -1,2 +1,5 @@
 //Web API can be accessed in the server side code in .NET and also on client side using JavaScript frameworks such as jQuery, AnguarJS, KnockoutJS etc.
 ========Consume Web API in ASP.NET MVC
+To consume Web API in ASP.NET MVC server side we can use HttpClient in the MVC controller. 
+HttpClient sends a request to the Web API and receives a response. 
+We then need to convert response data that came from Web API to a model and then render it into a view.
